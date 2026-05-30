55aec601bd917fcbe9920a5855acbb1f19c8b960
tarikthedev <tarikthedev@email.com>
2026-05-30 14:06:53 +0300

    Raket ve top nesneleri olusturulup ekrana cizdirildi

--- main.c
+++ main.c
@@ -5,8 +5,18 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
+typedef struct {
+    float x, y;
+    float width, height;
+} Paddle;
+
+typedef struct {
+    float x, y;
+    float size; 
+} Ball;
+
 int main(int argc, char* argv[]) {
-    
+
     (void)argc;
     (void)argv;
 
@@ -18,19 +28,32 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = SDL_CreateWindow("Breakout - YZM104", 
                                           SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
                                           WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
+    if (!window) {
         printf("Pencere olusturulamadi! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
         return -1;
     }
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
+    if (!renderer) {
         printf("Renderer olusturulamadi! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
         return -1;
     }
+
+    Paddle paddle = {
+        .width = 100, 
+        .height = 15,
+        .x = (WINDOW_WIDTH / 2.0f) - 50,
+        .y = WINDOW_HEIGHT - 40      
+    };
+
+    Ball ball = {
+        .size = 15,
+        .x = (WINDOW_WIDTH / 2.0f) - 7.5f,
+        .y = WINDOW_HEIGHT / 2.0f
+    };
 
     bool isRunning = true;
     SDL_Event event;
@@ -44,6 +67,14 @@ int main(int argc, char* argv[]) {
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_Rect paddleRect = {(int)paddle.x, (int)paddle.y, (int)paddle.width, (int)paddle.height};
+        SDL_RenderFillRect(renderer, &paddleRect);
+
+        SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255);
+        SDL_Rect ballRect = {(int)ball.x, (int)ball.y, (int)ball.size, (int)ball.size};
+        SDL_RenderFillRect(renderer, &ballRect);
 
         SDL_RenderPresent(renderer);
         
